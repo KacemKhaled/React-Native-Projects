244eba03b55661cc1ad0cca9ee585ce549371a12
kacemkhaled <kcm.khaled@gmail.com>
2019-12-23 16:07:14 -0500

    added template for react components

---
+++ template.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ComponentTemplate = props => {
+	return (
+
+		);
+};
+
+const styles = StyleSheet.create({});
+
+export default ComponentTemplate;
