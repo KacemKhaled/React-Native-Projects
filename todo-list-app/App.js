ed5acfd4b49f9583c1135329845af05be709958d
kacemkhaled <kcm.khaled@gmail.com>
2019-12-23 16:05:24 -0500

    final changes

--- todo-list-app/App.js
+++ todo-list-app/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 import TodoItem from './components/TodoItem';
 import TodoInput from './components/TodoInput';
 import ItemInput from './components/TodoInput';
 export default function App() {
   const [todoItems, setTodoItems] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
   const addItemHandler = todoTitle => {
     //console.log(enteredItem);
@@ -13,6 +14,7 @@ export default function App() {
       ...currentItems,
       { id: Math.random().toString(), value: todoTitle }
     ]);
+    setIsAddMode(false);
   };
 
   const removeItemHandler = itemId => {
@@ -21,17 +23,22 @@ export default function App() {
     });
   };
 
+  const cancelItemAdditionHandler = () => {
+    setIsAddMode(false);
+  };
+
   return (
     <View style={styles.screen}>
-      <ItemInput onAddTodoItem={addItemHandler} />
+      <Button title="Add New Todo Item" onPress={() => setIsAddMode(true)} />
+      <ItemInput visible={isAddMode} onAddTodoItem={addItemHandler} onCancel={cancelItemAdditionHandler} />
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={todoItems}
         renderItem={itemData => (
-          <TodoItem 
-            id={itemData.item.id} 
-            onDelete={removeItemHandler} 
-            title={itemData.item.value} 
+          <TodoItem
+            id={itemData.item.id}
+            onDelete={removeItemHandler}
+            title={itemData.item.value}
           />
         )}
       />
--- todo-list-app/components/TodoInput.js
+++ todo-list-app/components/TodoInput.js
@@ -1,36 +1,63 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Modal } from 'react-native';
 const ItemInput = props => {
     const [enteredItem, setEnteredItem] = useState('');
 
     const itemInputHandler = (enteredText) => {
         setEnteredItem(enteredText);
-      };
+    };
+
+    const addItemHandler = () => {
+        props.onAddTodoItem(enteredItem);
+        setEnteredItem('');
+    };
+
 
     return (
-        <View style={styles.inputContainer} >
-            <TextInput
-                placeholder="TODO item"
-                style={styles.input}
-                onChangeText={itemInputHandler}
-                value={enteredItem}
-            />
-            <Button title="ADD" onPress={props.onAddTodoItem.bind(this, enteredItem)} />
-        </View>
+        <Modal visible={props.visible} animationType="slide" >
+            <View style={styles.inputContainer} >
+                <TextInput
+                    placeholder="TODO item"
+                    style={styles.input}
+                    onChangeText={itemInputHandler}
+                    value={enteredItem}
+                />
+
+                <View style={styles.buttonsContainer}>
+                    <View style={styles.button}>
+                        <Button title="CANCEL" color="red" onPress={props.onCancel} />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="ADD" onPress={addItemHandler} />
+                    </View>
+                </View>
+            </View>
+        </Modal>
     );
 };
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        //flexDirection: 'row', //default is column
+        flex: 1, //to take as much space as its parent element gives it, otherwise if we don't add it it will take as much space as the child element do
+        justifyContent: 'center',
         alignItems: 'center'
+    },
+    button: {
+        width: '40%' //of the parentn
+    },
+    buttonsContainer: {
+        flexDirection: 'row', //default is column
+        justifyContent: 'space-around',
+        width: '60%'
     },
     input: {
         width: '80%',
+        color: 'black',
         borderBottomColor: 'black',
         borderWidth: 1,
-        padding: 10
+        padding: 10,
+        marginBottom: 10
     }
 });
 
