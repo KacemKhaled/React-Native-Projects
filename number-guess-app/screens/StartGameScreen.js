1744e6636f766682b88bcc36edc0fa26c05cb11c
kacemkhaled <kcm.khaled@gmail.com>
2019-12-23 16:06:32 -0500

    app to guess a number

---
+++ number-guess-app/screens/StartGameScreen.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+const StartGameScreen = props => {
+    return(
+        <View style={styles.screen} >
+            <Text style={styles.title}>Start a New Game!</Text>  
+            <View style={styles.inputContainer} >
+                <Text>Select a Number</Text>
+                <TextInput />
+                <View style={styles.buttonContainer}>
+                    <Button title="Reset" onPress={() => {}} />
+                    <Button title="Confirm" onPress={() => {}} />
+                </View>
+            </View>      
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+        //alignContent: 'flex-start' //default no need to set it
+    },
+    title:{
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    inputContainer:{
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+        shadowColor: 'black', //shadow works only on iOS
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        elevation: 8, //only on android to have a shadow
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10
+    },
+    buttonContainer:{
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    }
+});
+
+export default StartGameScreen;
