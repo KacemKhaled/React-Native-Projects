363f874a0ddc585be666f9acab3cf84cfcd6595f
kacemkhaled <kcm.khaled@gmail.com>
2019-12-31 14:10:07 -0500

    displaying a load spinner and handling errors

--- shop-app/store/actions/products.js
+++ shop-app/store/actions/products.js
@@ -8,28 +8,36 @@ export const SET_PRODUCTS = "SET_PRODUCTS";
 export const fetchProducts = () => {
   return async dispatch => {
     // any async code you want!
-    const response = await fetch(
-      "https://shop-app-rn-guide.firebaseio.com/products.json"
-    );
-
-    const resData = await response.json();
-    const loadedProducts = [];
-
-    for (const key in resData) {
-      loadedProducts.push(
-        new Product(
-          key,
-          "u1",
-          resData[key].title,
-          resData[key].imageUrl,
-          resData[key].description,
-          resData[key].price
-        )
+    try {
+      const response = await fetch(
+        "https://shop-app-rn-guide.firebaseio.com/products.json"
       );
 
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const resData = await response.json();
+      const loadedProducts = [];
+
+      for (const key in resData) {
+        loadedProducts.push(
+          new Product(
+            key,
+            "u1",
+            resData[key].title,
+            resData[key].imageUrl,
+            resData[key].description,
+            resData[key].price
+          )
+        );
+      }
+      // console.log(resData);
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    } catch (err) {
+      // send to custom analytics server
+      throw err;
     }
-    // console.log(resData);
-    dispatch({ type: SET_PRODUCTS, products: loadedProducts });
   };
 };
 
