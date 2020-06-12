5ecde7d7d7c946459eb56dd62fcb75b38a6712b1
kacemkhaled <kcm.khaled@gmail.com>
2020-06-11 22:58:59 -0400

    removed key

--- shop-app/store/actions/auth.js
+++ shop-app/store/actions/auth.js
@@ -17,7 +17,7 @@ export const authenticate = (userId, token, expiryTime) => {
 export const signup = (email, password) => {
   return async dispatch => {
     const response = await fetch(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDgdGjf2KqHqf18AsXAEhix1ppZJrZNWKc",
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=#############################",
       {
         method: "POST",
         headers: {
@@ -60,7 +60,7 @@ export const signup = (email, password) => {
 export const login = (email, password) => {
   return async dispatch => {
     const response = await fetch(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDgdGjf2KqHqf18AsXAEhix1ppZJrZNWKc",
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=#############################",
       {
         method: "POST",
         headers: {
